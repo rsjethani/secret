983a905e04f8ff2ea3d4e380ebcfa01f5b5fb663
Ravi Shekhar Jethani <rsjethani@gmail.com>
2022-01-12 21:51:11 +0100

    Add documentation for public types
    Also add internal comments.
    
    Closes #5

--- secret.go
+++ secret.go
@@ -5,58 +5,85 @@ import (
 	"fmt"
 )
 
+// DefaultRedact is used by default if no other redact hint is given.
 const DefaultRedact string = "*****"
 
+// Text provides a way to store your secret value and a corresponding redact hint.
 type Text struct {
+	// v is the actual secret values.
 	v *string
+	// r is the redact hint to be used in place of secret.
 	r *string
 }
 
+// NewText creates a new Text instance with s as the secret value. Multiple option functions can
+// be passed to alter default behavior.
 func NewText(s string, options ...func(*Text)) Text {
-	sec := Text{}
+	sec := Text{
+		v: new(string),
+		r: new(string),
+	}
 
-	sec.init()
 	*sec.v = s
+	*sec.r = DefaultRedact
 
+	// Apply options to override defaults
 	for _, opt := range options {
 		opt(&sec)
 	}
+
 	return sec
 }
 
-func (s *Text) init() {
-	s.v = new(string)
-	s.r = new(string)
-	*s.r = DefaultRedact
-}
-
-func CustomRedact(r string) func(*Text) {
-	return func(s *Text) {
-		*s.r = r
-	}
-}
-
+// String implements the fmt.Stringer interface and returns only the redact hint. This prevents the
+// secret value from being to std*, logs etc.
 func (s Text) String() string {
 	return *s.r
 }
 
+// Value gives you access to the actual secret value stored inside Text.
 func (s Text) Value() string {
 	return *s.v
 }
 
+// MarshalJSON allows Text to be serialized into a JSON string. Only the redact hint is part of the
+// the JSON string.
 func (s Text) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`"%s"`, *s.r)), nil
 }
 
+// UnmarshalJSON allows a JSON string to be deserialized into a Text value. DefaultRedact is set
+// as the redact hint.
 func (s *Text) UnmarshalJSON(b []byte) error {
-	s.init()
-	return json.Unmarshal(b, s.v)
+	// Get the new secret value from unmarshalled data.
+	var n string
+	if err := json.Unmarshal(b, &n); err != nil {
+		return err
+	}
+
+	// If the original redact is not nil then use it otherwise fallback to default.
+	if s.r != nil {
+		*s = NewText(n, CustomRedact(*s.r))
+	} else {
+		*s = NewText(n)
+	}
+
+	return nil
 }
 
+// Redacted sets "[REDACTED]" as the redact hint.
 func Redacted(s *Text) {
 	*s.r = "[REDACTED]"
 }
 
+// FiveXs sets "XXXXX" as the redact hint.
 func FiveXs(s *Text) {
 	*s.r = "XXXXX"
+}
+
+// CustomRedact sets the value of r as the redact hint.
+func CustomRedact(r string) func(*Text) {
+	return func(s *Text) {
+		*s.r = r
+	}
 }
--- secret_test.go
+++ secret_test.go
@@ -5,26 +5,13 @@ import (
 	"testing"
 )
 
-func TestText_secret_and_its_copy_share_the_same_data(t *testing.T) {
-	s1 := NewText("hello")
-	s2 := s1
-
-	if s2.r != s1.r {
-		t.Fatal("redact field of the secret and its copy do not point to same string")
-	}
-
-	if s2.v != s1.v {
-		t.Fatal("value field of the secret and its copy do not point to same string")
-	}
-}
-
 func TestText_UnmarshalJSON_allocates_new_data_rather_than_overwiting_existing(t *testing.T) {
 	s1 := NewText("hello")
 
 	oldRedact := s1.r
 	oldValue := s1.v
 
-	err := json.Unmarshal([]byte(`"hell"`), &s1)
+	err := json.Unmarshal([]byte(`"hello"`), &s1)
 	if err != nil {
 		t.Fatal(err)
 	}
